35f3d31daf7b2bd377e5e6530fde693f90c88eb5
Rupali Khobarkhede <mailtorupalik@gmail.com>
2012-03-29 16:57:24 +0530

    barnyard_controller added

--- my_app/app/controllers/barnyard_controller.rb
+++ my_app/app/controllers/barnyard_controller.rb
@@ -1,6 +1,13 @@
 class BarnyardController < ApplicationController
   def noises
-     first_animal=Animal.new
-     @sound1=first_animal.make_noise  
+
+     @first_animal=Animal.new
+	@first_animal.name='cow'
+     @first_animal.noise='Moo!!'     
+
+     @second_animal=Animal.new
+	@second_animal.name='pig'     
+	@second_animal.noise='Oink!!'     
+	
 end
 end
--- my_app/app/models/animal.rb
+++ my_app/app/models/animal.rb
@@ -1,5 +1,13 @@
 class Animal  #< ActiveRecord::Base -- bcoz of this we hav to connect to DB
-def make_noise   
-  'Moo!'
+
+attr_accessor :name
+
+def noise   
+  @noise
 end
+
+def noise=(new_noise)
+   @noise=new_noise
+end
+	
 end
